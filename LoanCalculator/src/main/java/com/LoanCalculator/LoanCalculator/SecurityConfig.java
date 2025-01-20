b7c5537d38fbc6acac1a9f5c0678a2cd22621e55
Stellahchepchumba <stellahchepchumba96@gmail.com>
2025-01-20 17:05:47 +0300

    security file update

--- LoanCalculator/src/main/java/com/LoanCalculator/LoanCalculator/SecurityConfig.java
+++ LoanCalculator/src/main/java/com/LoanCalculator/LoanCalculator/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.LoanCalculator.LoanCalculator;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -16,20 +14,6 @@ public class SecurityConfig {
     public SecurityConfig(CorsProperties corsProperties) {
         this.corsProperties = corsProperties;
     }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/loans/calculate").permitAll()
-                        .anyRequest().authenticated() // Restrict other endpoints
-                );
-
-        return http.build();
-    }
-
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
