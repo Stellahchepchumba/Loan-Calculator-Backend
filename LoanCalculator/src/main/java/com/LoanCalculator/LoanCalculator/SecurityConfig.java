788258faccde5f37b3c1979519c5b4c0ede3ef9e
Stellahchepchumba <stellahchepchumba96@gmail.com>
2025-01-20 11:35:53 +0300

    security file update

--- LoanCalculator/src/main/java/com/LoanCalculator/LoanCalculator/SecurityConfig.java
+++ LoanCalculator/src/main/java/com/LoanCalculator/LoanCalculator/SecurityConfig.java
@@ -3,10 +3,11 @@ package com.LoanCalculator.LoanCalculator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -30,15 +31,20 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000", 
+                "http://localhost:3000",
                 "https://loan-calculator-3opx2xvt5-stellahs-projects.vercel.app"
         ));
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true); // Enable credentials
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 }
